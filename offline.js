7bf708623c9a10b8eedb1c32c20590d64e06c08a
johin21101 <130030633+johin21101@users.noreply.github.com>
2023-04-06 14:19:37 +0800

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1680761393,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/sprite3-sheet1.png",
+		"images/sprite4-sheet0.png",
+		"images/sprite5-sheet0.png",
+		"media/mixkit-aggressive-beast-roar-13.m4a",
+		"media/mixkit-aggressive-beast-roar-13.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
